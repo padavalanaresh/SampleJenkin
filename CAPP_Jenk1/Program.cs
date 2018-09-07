ef46755e90738a0928338c038bd15d62edad5128
padavalanaresh <NareshPadavala@synovus.com>
2018-09-07 14:27:02 -0400

    Sample Code

---
+++ CAPP_Jenk1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAPP_Jenk1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Heloo this is my sample Jenkin Deploy.....");
+
+			Console.WriteLine("Hit enter to exit()");
+
+			Console.Read();
+		}
+	}
+}
